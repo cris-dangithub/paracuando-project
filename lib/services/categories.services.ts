985a74e11a0e61a4ca281791151a451d9c5cda32
cris-dangithub <cristiandaniel8080@gmail.com>
2023-04-16 15:09:54 -0500

    feat: categories service

--- lib/services/categories.services.ts
+++ lib/services/categories.services.ts
@@ -1,15 +1,35 @@
 import useSWR from 'swr';
-import { CategoriesResponse } from '../interfaces/categories.interface';
+import axios from '../helpers/axios.helper.';
+import {
+  CategoriesResponse,
+  CategoryPutRequest,
+} from '../interfaces/categories.interface';
 function useCategories() {
   const { data, error, isLoading, mutate } = useSWR<CategoriesResponse>(
     '/publications-types'
   );
   return {
-    data: data,
-    error,
-    isLoading,
-    mutate,
+    categories: data,
+    errCategories: error,
+    loaderCategories: isLoading,
+    mutCategories: mutate,
   };
 }
 
-export { useCategories };
+function useCategoryById(publicationType: string) {
+  const { data, error, isLoading, mutate } = useSWR(
+    `/publications-types/${publicationType}`
+  );
+  return {
+    category: data,
+    errCategory: error,
+    loaderCategory: isLoading,
+    mutCategory: mutate,
+  };
+}
+
+function updateCategory(publicationType: string, data: CategoryPutRequest) {
+  return axios.put(`/publications-types/${publicationType}`, data);
+}
+
+export { useCategories, useCategoryById, updateCategory };
