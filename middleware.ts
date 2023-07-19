68456b9f1913987e070b8a74d8f9a7d70a1f50ac
cris-dangithub <cristiandaniel8080@gmail.com>
2023-07-18 22:26:58 -0500

    fix: middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getConfig } from './lib/helpers/getConfig';
-import { routeChecker } from './lib/helpers/middleware.helpers';
-import { BASE_URL } from './lib/services/BASE_URL';
 
 export async function middleware(request: NextRequest) {
-  const protectWithJWT = ['/profile/:path*', '/create-event'];
-  const protectWithoutJWT = ['/log-in', '/sign-up'];
-  const pathname = request.nextUrl.pathname;
   const response = NextResponse.next();
   if (process.env.NODE_ENV === 'development') {
-    //return NextResponse.next();
+    return NextResponse.next();
   }
 
   const jwt = request.cookies.get('token');
 
-  // Verificar que el token sea válido
-  if (jwt?.value) {
-    const user = await fetch(`${BASE_URL}/auth/me`, getConfig(jwt?.value));
-    if (!user.ok) response.cookies.delete('token');
-  }
-
-  // Si el usuario es váido e intenta entrar a los logeos
-  if (jwt?.value && routeChecker(pathname, protectWithoutJWT)) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  // Si usuario no válido, analizar rutas que necesitan de protección de usuario
-  if (!jwt?.value && routeChecker(pathname, protectWithJWT)) {
+  if (!jwt) {
     return NextResponse.redirect(new URL('/log-in', request.url));
   }
-
   return response;
 }
 
 export const config = {
-  matcher: ['/profile/:path*', '/create-event', '/log-in', '/sign-up'],
+  matcher: ['/profile/:path*', '/create-event'],
 };
